205d6651260270bdf576c7d9b6dd97bdb1ebe1c3
Theophilus Nettey <theophilus.nettey@st.vvu.edu.gh>
2017-03-16 12:43:37 +0000

    styled home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.theme')
 
 @section('working_area')
+    @if(empty($articles))
+        <div class="card">
+            <p>There are no articles.</p>
+        </div>
+    @endif
     @if(!empty($articles))
+
 
         @foreach($articles as $article)
     <div class="col-lg-4">
         <div class="portlet">
             <div class="portlet-heading bg-primary">
                 <h3 class="portlet-title">
-                    {{$article->title}}
+                    <a href="/article/{{$article->id}}">{{$article->title}}</a>
                 </h3>
                 <div class="portlet-widgets">
                     <a href="javascript:;" data-toggle="reload"><i class="zmdi zmdi-refresh"></i></a>
@@ -19,15 +25,26 @@
             </div>
             <div id="bg-primary1" class="panel-collapse collapse in">
                 <div class="portlet-body">
-                        {{$article->body}}
-                </div>
-                <hr>
+                        {{$article->description}}
 
-                <p>Written by: {{$article->user->name}}</p>
+                    <hr>
+                    Written by: {{$article->user->name}}
+                    <p>
+                        @if($article->user_id == $article->user->id)
+                            <span class="pull-right"><a href="/article/{{$article->id}}/edit">Edit</a></span>
+                            <form action="/article/{{$article->id}}" method="POST">
+                                {{csrf_field()}}
+                                {{method_field('DELETE')}}
+                                <button class="btn btn-sm btn-warning" type="submit">Delete your post</button>
+                            </form>
+                        @endif
+                    </p>
+                </div>
             </div>
         </div>
+
     </div>
-@endforeach
+        @endforeach
     @endif
 
 
