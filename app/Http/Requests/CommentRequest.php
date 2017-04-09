9cb1f0f6d382a3187634e8727e1e7b09cb96c364
Theophilus Nettey <theophilus.nettey@st.vvu.edu.gh>
2017-04-09 09:13:35 +0100

    created comment request

---
+++ app/Http/Requests/CommentRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'body'=> 'required|min:2'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'body.required'  => 'The content of your comment should exceed 1 character.',
+        ];
+    }
+}
